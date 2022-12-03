ae8952c25f9e5da4b504178bdcbc965de8055052
Dmitry Kuchukov <kudim84@gmail.com>
2022-12-03 16:46:16 +0300

    feat: implement copy folder

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,32 @@
-const copy = async () => {
-    // Write your code here 
-};
+import { join } from 'path'
+import { __dirname } from '../utils.js'
+import { readdir, mkdir, copyFile, unlink } from 'fs/promises'
+import { existsSync } from 'fs'
 
-copy();
+const copy = async () => {
+  const fileCopy = join(__dirname, 'fs/files_copy')
+  const dir = join(__dirname, 'fs/files')
+
+  try {
+    if (existsSync(fileCopy) || !existsSync(dir)) {
+      throw new Error('FS operation failed')
+    }
+
+    await mkdir(fileCopy)
+    const files = await readdir(dir)
+    const copiedFiles = await readdir(fileCopy)
+    if (copyFile.length !== 0) {
+      for (const file of copiedFiles) {
+        unlink(join(__dirname, 'fs/files-copy', file))
+      }
+    }
+    for (const file of files) {
+      await copyFile(join(__dirname, 'fs/files', file), join(fileCopy, file))
+    }
+    console.log('Files copied')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+copy()
