9c6afa67dc2183d310196552d9e0075d70a3cb44
Dmitry Kuchukov <kudim84@gmail.com>
2022-12-03 10:43:51 +0300

    feat: implement function create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
-const create = async () => {
-    // Write your code here 
-};
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+import { createWriteStream, existsSync } from 'fs'
 
-await create();
+const create = async () => {
+  const __dirname = dirname(fileURLToPath(import.meta.url))
+
+  const file = join(__dirname, './files/fresh.txt')
+  const data = 'I am fresh and young'
+  if (existsSync(file)) {
+    throw new Error('FS operation failed')
+  }
+  let wstream = createWriteStream(file)
+  wstream.write(data)
+}
+
+await create()
