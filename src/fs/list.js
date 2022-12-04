cc86fe0c3075b85ae738b806028ae0c70d28fa0b
Dmitry Kuchukov <kudim84@gmail.com>
2022-12-04 10:59:01 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
-const list = async () => {
-    // Write your code here 
-};
+import { readdir } from 'fs/promises'
+import { __dirname } from '../utils.js'
+import { join } from 'path'
+import { existsSync } from 'fs'
 
-await list();
+const list = async () => {
+  try {
+    const folder = join(__dirname, 'fs', 'files')
+    if (!existsSync(folder)) {
+      throw new Error('FS operation failed')
+    }
+    const files = await readdir(folder)
+    console.log(files)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+await list()
