3ef17816ca3feaf7b5b5aaeb8c1c4f8fef42a4f0
Dmitry Kuchukov <kudim84@gmail.com>
2022-12-04 11:26:08 +0300

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
-const rename = async () => {
-    // Write your code here 
-};
+import { rename as setNewName } from 'fs/promises'
+import { __dirname } from '../utils.js'
+import { join } from 'path'
+import { existsSync } from 'fs'
 
-await rename();
+const rename = async () => {
+  const oldFile = join(__dirname, 'fs', 'files', 'wrongFilename.txt')
+  const newFile = join(__dirname, 'fs', 'files', 'properFilename.md')
+  try {
+    if (!existsSync(oldFile) || existsSync(newFile)) {
+      throw new Error('FS operation failed')
+    }
+    await setNewName(oldFile, newFile)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+await rename()
