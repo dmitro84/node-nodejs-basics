1309badae332a6a4c8bb061202f90d06f2fc7b73
Dmitry Kuchukov <kudim84@gmail.com>
2022-12-04 11:31:02 +0300

    feat: implement read function in streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
-const read = async () => {
-    // Write your code here 
-};
+import { createReadStream } from 'fs'
+import { join } from 'path'
+import { stdout } from 'process'
+import { __dirname } from '../utils.js'
 
-await read();
+const read = async () => {
+  let data = ''
+  const file = join(__dirname, 'streams', 'files', 'fileToRead.txt')
+  const readStream = createReadStream(file, 'utf-8')
+
+  readStream.on('data', (chunk) => (data += chunk))
+  readStream.on('end', () => stdout.write(data))
+  readStream.on('error', (error) => console.log('Error', error.message))
+}
+
+await read()
