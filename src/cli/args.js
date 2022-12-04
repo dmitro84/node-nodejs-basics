4882e859eeef052a95af1095ca7ecbcc8b0a6fe9
Dmitry Kuchukov <kudim84@gmail.com>
2022-12-04 15:42:56 +0300

    refactor: refactor parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,8 +1,10 @@
 const parseArgs = () => {
-    const argvList = process.argv.slice(2)
-    for(let i=0; i<argvList.length; i+=2) {
-        console.log(`${argvList[i].slice(2)} is ${argvList[i+1]}`);
-    }
-};
+  const argvList = process.argv.slice(2)
+  let str = ''
+  for (let i = 0; i < argvList.length; i += 2) {
+    str += `${argvList[i].slice(2)} is ${argvList[i + 1]}, `
+  }
+  console.log(str.slice(0, str.length - 2))
+}
 
-parseArgs();
+parseArgs()
