fa3fa4ac93ee59ce763c13327551d5a2d5be509a
Dmitry Kuchukov <kudim84@gmail.com>
2022-12-04 15:09:32 +0300

    feat: implement parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,8 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argvList = process.argv.slice(2)
+    for(let i=0; i<argvList.length; i+=2) {
+        console.log(`${argvList[i].slice(2)} is ${argvList[i+1]}`);
+    }
 };
 
 parseArgs();
