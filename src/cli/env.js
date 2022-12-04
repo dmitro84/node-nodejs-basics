3ddb9def3e28853cbce01c4e84b68d164e18dc23
Dmitry Kuchukov <kudim84@gmail.com>
2022-12-04 15:55:16 +0300

    feat: implement parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
-};
+  const rssList = Object.entries(process.env).reduce((acc, [key, value]) => {
+    if (key.startsWith('RSS_')) {
+      acc.push(`${key}=${value}`)
+    }
+    return acc
+  }, [])
 
-parseEnv();
+  console.log(rssList.reverse().join('; '))
+}
+
+parseEnv()
