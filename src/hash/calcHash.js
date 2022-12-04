ee8f12e9d2c6d0b9a6ca1086a73e023707ccdee9
Dmitry Kuchukov <kudim84@gmail.com>
2022-12-04 14:29:51 +0300

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { createHmac } from 'crypto'
+import { createReadStream } from 'fs'
+import { join } from 'path'
+import { stdout } from 'process'
+import { __dirname } from '../utils.js'
 
-await calculateHash();
+const calculateHash = async () => {
+  let data = ''
+  const file = join(__dirname, 'hash', 'files', 'fileToCalculateHashFor.txt')
+  const secret = 'secret'
+  const sha256Hasher = createHmac('sha256', secret);
+
+  const readStream = createReadStream(file, 'utf-8')
+
+  readStream.on('data', (chunk) => (data += chunk.toString()))
+  readStream.on('end', () => stdout.write(sha256Hasher.update(data).digest('hex')))
+  readStream.on('error', (error) => console.log('Error', error.message))
+}
+
+await calculateHash()
