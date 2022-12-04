99d1c748dd00bfaab0649113445f11974322e7d4
Dmitry Kuchukov <kudim84@gmail.com>
2022-12-04 11:08:30 +0300

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
-const read = async () => {
-    // Write your code here 
-};
+import { createReadStream, existsSync } from 'fs'
+import { join } from 'path'
+import { stdout } from 'process'
+import { __dirname } from '../utils.js'
 
-await read();
+const read = async () => {
+  let data = ''
+  const file = join(__dirname, 'fs', 'files', 'fileToRead.txt')
+  if (!existsSync(file)) {
+    throw new Error('FS operation failed')
+  }
+  const readStream = createReadStream(file, 'utf-8')
+
+  readStream.on('data', (chunk) => (data += chunk))
+  readStream.on('end', () => stdout.write(data))
+  readStream.on('error', (error) => console.log('Error', error.message))
+}
+
+await read()
