9be3b9baf7b31478bad3492c41bf8601b6dc67f2
dmitry.kuchukov <kudim84@gmail.com>
2022-12-06 00:40:15 +0300

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import { cpus } from 'os'
+import { __dirname } from '../utils.js'
+import { join } from 'path'
+import { Worker } from 'worker_threads'
 
-await performCalculations();
+const performCalculations = async () => {
+  const cpuNumbers = cpus()
+  let number = 10
+
+  const workerPromises = await Promise.allSettled(
+    cpuNumbers.map(() => {
+      return new Promise((resolve, reject) => {
+        const worker = new Worker(join(__dirname, 'wt', 'worker.js'), {
+          workerData: number++,
+        })
+
+        worker.once('message', (msg) => resolve(msg))
+        worker.once('err', (err) => reject(err))
+      })
+    })
+  )
+
+  const results = workerPromises.map(({ status, value }) => {
+    return {
+      status: status === 'fulfilled' ? 'resolved' : 'error',
+      data: status === 'fulfilled' ? value : null,
+    }
+  })
+
+  console.log(results)
+}
+
+await performCalculations()
