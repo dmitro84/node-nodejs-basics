670d8e20410c57bf4ad1d15bf68722ffbb9bf91c
dmitry.kuchukov <kudim84@gmail.com>
2022-12-05 21:34:46 +0300

    feat: implement cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,15 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { ChildProcess, fork } from 'child_process'
+import { __dirname } from '../utils.js'
+import { join } from 'path'
 
-spawnChildProcess();
+const spawnChildProcess = async (args) => {
+  const myChildProcess = fork(join(__dirname, 'cp', 'files', 'script.js'), [
+    ...args.split(' '),
+  ])
+
+  myChildProcess.on('message', (msg) => {
+    console.log(msg)
+  })
+}
+
+spawnChildProcess('--arg1 --arg2 --arg3')
